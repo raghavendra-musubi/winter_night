62878aec1073dfc9e8ca93a6c907889890b69878
raghavendra-musubi <raghavendra@techis.io>
2020-10-25 02:23:14 +0530

    rebased commit

---
+++ blog/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+from django.views.generic import ListView
+from .models import Post
+
+
+
+# posts = [
+#     {
+#         'author':'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author':'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'First post content',
+#         'date_posted': 'August 28, 2018'
+#     },
+# ]
+
+# Create your views here.
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+# class PostListView(ListVIew):
+#     model = Post
+
+def about(request):
+    return render(request, 'blog/about.html', { 'title': 'About'})
+
