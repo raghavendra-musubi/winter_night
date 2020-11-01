301377b0eb7abd4fd905baf5fd947d58944b62ad
raghavendra-musubi <raghavendra@techis.io>
2020-11-01 20:58:19 +0530

    blog post detail view works

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
-from .views import PostListView
+from .views import (
+    PostListView,
+    PostDetailView,
+)
 
 urlpatterns = [
     # path('', views.home, name="blog-home"), # for function-based home 
     path('',PostListView.as_view(), name='blog-home'),
+    path('/post/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name="blog-about"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
 
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    )
 from .models import Post
 
 
@@ -14,12 +17,16 @@ from .models import Post
 #     }
 #     return render(request, 'blog/home.html', context)
 
+
+def about(request):
+    return render(request, 'blog/about.html', { 'title': 'About'})
+
 class PostListView(ListView):
     model = Post
     template_name = 'blog/home.html'
     context_object_name = 'posts' # sets the context variable name inside the template being called
     ordering = ["-date_posted"]
 
-def about(request):
-    return render(request, 'blog/about.html', { 'title': 'About'})
-
+# single line Class view
+class PostDetailView(DetailView):
+    model = Post
