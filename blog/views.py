39df55fd03524f872bbc81409943cff456040871
raghavendra-musubi <raghavendra@techis.io>
2020-11-02 04:00:45 +0530

    create-edit-delete- post works!

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    )
+    UpdateView,
+    DeleteView
+)
+
 from .models import Post
 
-
-# Create your views here.
-
-# # function-based view
-# def home(request):
-#     context = {
-#         'posts': Post.objects.all()
-#     }
-#     return render(request, 'blog/home.html', context)
 
 
 def about(request):
@@ -41,3 +38,25 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author: return True   
+        else: return False 
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author: return True
+        else: return False
