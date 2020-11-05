aa28d17a4f15957682c7d8e614b2fc894b9d0271
raghavendra-musubi <raghavendra@techis.io>
2020-11-06 04:32:15 +0530

    filtering and pagination

--- blog/urls.py
+++ blog/urls.py
@@ -6,11 +6,13 @@ from .views import (
     PostCreateView,
     PostUpdateView,
     PostDeleteView,
+    UserPostListView,
 )
 
 urlpatterns = [
     # path('', views.home, name="blog-home"), # for function-based home 
     path('',PostListView.as_view(), name='blog-home'),
+    path('user/<str:username>',UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/',PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update',PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete',PostDeleteView.as_view(), name='post-delete'),
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
     UserPassesTestMixin
@@ -25,7 +27,18 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'posts' # sets the context variable name inside the template being called
     ordering = ["-date_posted"]
+    paginate_by = 5
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts' # sets the context variable name inside the template being called
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')        
+        
 # Single Line Class View
 # this uses only the default conventions
 class PostDetailView(DetailView):
